48ee00796e96438a5e41b9ae438b8be29d2cabba
Xelfie <d.marine99@yahoo.co.uk>
2024-07-21 13:28:45 +0400

    Added environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require("express");
 const app = express();
-const port = 8000;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Example app listening on port ${process.env.SERVER_PORT}`);
 });
